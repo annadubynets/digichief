b73e7c39a68b9f872ccbabf811411405c809bd70
Anna Dubynets <arogulina@gmail.com>
2021-04-22 14:42:25 +0300

    fix menu (mobile)

--- js/script.js
+++ js/script.js
@@ -95,3 +95,21 @@ if ($(".accordion").length > 0) {
         }
     }
 }
+
+window.addEventListener('DOMContentLoaded', () => {
+    const menu = document.querySelector('.menu'),
+        menuItem = document.querySelectorAll('.menu-item'),
+        hamburger = document.querySelector('.hamburger');
+
+    hamburger.addEventListener('click', () => {
+        hamburger.classList.toggle('hamburger-active');
+        menu.classList.toggle('menu-active');
+    });
+
+    menuItem.forEach(item => {
+        item.addEventListener('click', () => {
+            hamburger.classList.toggle('hamburger-active');
+            menu.classList.toggle('menu-active');
+        })
+    })
+})
